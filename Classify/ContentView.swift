eef10ffc2285b5ffcfafe8971cd8d257de7f6904
FelBenitez <febenitez953@gmail.com>
2024-10-12 17:08:53 -0500

    Test

--- Classify/ContentView.swift
+++ Classify/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
 
     var body: some View {
         NavigationSplitView {
-            List {
+            List { Hello
                 ForEach(items) { item in
                     NavigationLink {
                         Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
